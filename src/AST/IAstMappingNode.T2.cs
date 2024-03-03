6887672c20c00d32bc2ec54f3f5752d12991ae30
nootshell <20719994+nootshell@users.noreply.github.com>
2024-03-03 03:01:22 +0100

    Rename Key/Value properties on IAstMappingNode to KeyNode and ValueNode, respectively

--- src/AST/IAstMappingNode.T2.cs
+++ src/AST/IAstMappingNode.T2.cs
@@ -2,8 +2,8 @@ namespace Ntsh.Serialization.AST {
 
 	public interface IAstMappingNode<out TKeyNode, out TValueNode> : IAstNode where TKeyNode : IAstNode where TValueNode : IAstNode {
 
-		TKeyNode Key { get; }
-		TValueNode Value { get; }
+		TKeyNode KeyNode { get; }
+		TValueNode ValueNode { get; }
 
 	}
 
