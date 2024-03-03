148100843f9b7c2b5799214610dad266f51e9e46
nootshell <20719994+nootshell@users.noreply.github.com>
2024-03-03 03:01:03 +0100

    Add missing covariance to IAstMappingNode<TKeyNode, TValueNode>

--- src/AST/IAstMappingNode.T2.cs
+++ src/AST/IAstMappingNode.T2.cs
@@ -1,6 +1,6 @@
 namespace Ntsh.Serialization.AST {
 
-	public interface IAstMappingNode<TKeyNode, TValueNode> : IAstNode where TKeyNode : IAstNode where TValueNode : IAstNode {
+	public interface IAstMappingNode<out TKeyNode, out TValueNode> : IAstNode where TKeyNode : IAstNode where TValueNode : IAstNode {
 
 		TKeyNode Key { get; }
 		TValueNode Value { get; }
